3fc4cbc204b254d457051e712fb841a96f31a21f
Chanov Sergey <s.c.chanov@mail.ru>
2023-12-12 20:19:00 +0300

    initial commit

---
+++ django-channels-push-notification/core/core/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import path
+from notification import views
+from notification.consumers import NotificationConsumer
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", views.notification_page_view, name="notification_page")
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+websocket_urlpatterns = [
+    path("ws/notifications/", NotificationConsumer.as_asgi())
+]
